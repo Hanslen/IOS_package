17b54c6244bd183c4a6a42e049e633aa18bd8452
Jiahe Chen <chenjiaheyeah@gmail.com>
2017-06-22 10:50:51 +0800

    Add files via upload

---
+++ AFNetworking_extend/upload_file.php
@@ -0,0 +1,45 @@
+<?php
+// 允许上传的图片后缀
+$allowedExts = array("gif", "jpeg", "jpg", "png");
+$temp = explode(".", $_FILES["file"]["name"]);
+echo $_FILES["file"]["size"];
+$extension = end($temp);     // 获取文件后缀名
+if ((($_FILES["file"]["type"] == "image/gif")
+|| ($_FILES["file"]["type"] == "image/jpeg")
+|| ($_FILES["file"]["type"] == "image/jpg")
+|| ($_FILES["file"]["type"] == "image/pjpeg")
+|| ($_FILES["file"]["type"] == "image/x-png")
+|| ($_FILES["file"]["type"] == "image/png"))
+&& ($_FILES["file"]["size"] < 204800)   // 小于 200 kb
+&& in_array($extension, $allowedExts))
+{
+	if ($_FILES["file"]["error"] > 0)
+	{
+		echo "Error: " . $_FILES["file"]["error"] . "<br>";
+	}
+	else
+	{
+		echo "Upload Name: " . $_FILES["file"]["name"] . "<br>";
+		echo "File type: " . $_FILES["file"]["type"] . "<br>";
+		echo "File size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+		echo "Temp file location: " . $_FILES["file"]["tmp_name"] . "<br>";
+		
+		// 判断当期目录下的 upload 目录是否存在该文件
+		// 如果没有 upload 目录，你需要创建它，upload 目录权限为 777
+		if (file_exists("uploadtest/" . $_FILES["file"]["name"]))
+		{
+			echo $_FILES["file"]["name"] . " File exists ";
+		}
+		else
+		{
+			// 如果 upload 目录不存在该文件则将文件上传到 upload 目录下
+			move_uploaded_file($_FILES["file"]["tmp_name"], "uploadtest/" . $_FILES["file"]["name"]);
+			echo "Filst stored at: " . "uploadtest/" . $_FILES["file"]["name"];
+		}
+	}
+}
+else
+{
+	echo "Invalid file format";
+}
+?>
